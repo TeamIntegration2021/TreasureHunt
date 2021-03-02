55133aa7cdc4fe352717265a31e10182202569b0
jzaretsky427 <jackzaret427@gmail.com>
2021-03-02 18:11:51 -0500

    Created menu and combined files

---
+++ TreasureHuntPlayer/Menu.cpp
@@ -0,0 +1,20 @@
+//create menu class to display menu and ask for users name, then begin the game using their inputed name
+#include<iostream>
+#include<string>
+//#include "TreasureHuntPlayer.h"//
+#include "Menu.h"
+using namespace std;
+
+
+
+//create member function to display menu//
+void menu::DisplayMenu() {
+	cout << "Welcome to "; //insert game name when decided//
+		// insert paragraph about game??
+}
+
+void menu::askName() {
+	cout << "Please enter your name: ";
+	//cin >> playerName;//
+
+}
---
+++ TreasureHuntPlayer/Menu.h
@@ -0,0 +1,40 @@
+#include<iostream>
+#include "TreasureHuntPlayer.h"
+#include "TreasureHuntPlayer.cpp"
+using namespace std;
+
+#ifndef Menu_h
+#define Menu_h
+
+//create a class that will allow the main to call the functions inside
+class menu {
+
+
+
+	// no private member functions, so that other classes and functions can call/use
+public:
+	void DisplayMenu();
+	void askName();
+
+
+
+	//string playerName;//
+	//const timer = ?//
+	//char choice//
+	//void beginGame()//
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
